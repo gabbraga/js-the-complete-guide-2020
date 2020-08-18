f16f626988322319aa14a0b91c6ec9c7ae9755a1
Gab <gbraga@uwo.ca>
2020-08-18 09:45:18 -0400

    added log of calc description history

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 let calculationDescription = '';
 let currentResult = 0;
+let calculationLogs = [];
 
 function getUserInput() { 
     return parseInt(userInput.value); 
@@ -11,6 +12,7 @@ function calc(operator) {
     //Template literals (``) are string literals allowing embedded expressions. 
     //You can use multi-line strings and string interpolation features with them.
     calculationDescription = `${currentResult} ${operator} ${userInput}`;
+    logCalculation(calculationDescription);
 
     switch(operator) {
         case '+': { 
@@ -32,10 +34,16 @@ function calc(operator) {
     }
     outputResult(currentResult, calculationDescription);
 }
+
 function add() { calc('+'); }
 function sub() { calc('-'); }
 function mult() { calc('*');}
 function div() { calc('/'); }
+
+function logCalculation(desc){
+    calculationLogs.push(desc);
+    logHistory(calculationLogs[calculationLogs.length - 1]);
+}
 
 addBtn.addEventListener('click', add);
 subtractBtn.addEventListener('click', sub);
--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -3,6 +3,7 @@ const addBtn = document.getElementById('btn-add');
 const subtractBtn = document.getElementById('btn-subtract');
 const multiplyBtn = document.getElementById('btn-multiply');
 const divideBtn = document.getElementById('btn-divide');
+const historyLog = document.getElementById('log');
 
 const currentResultOutput = document.getElementById('current-result');
 const currentCalculationOutput = document.getElementById('current-calculation');
@@ -10,4 +11,10 @@ const currentCalculationOutput = document.getElementById('current-calculation');
 function outputResult(result, text) {
   currentResultOutput.textContent = result;
   currentCalculationOutput.textContent = text;
+}
+
+function logHistory(desc) {
+  let newLog = document.createElement('li');
+  newLog.innerHTML = desc
+  historyLog.appendChild(newLog);
 }
