a50ba705427a383b7707003b9c2865d24dc6dcbd
Gab <gbraga@uwo.ca>
2020-10-27 16:29:31 -0400

    validate movie details input

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,10 @@
 const addMovieDetailsBtn = document.getElementById("add-Movie");
 const addMovieModalDiv = document.getElementById('add-modal');
-const cancelAddMovieBtn = addMovieModalDiv.querySelector('.btn--passive');
+const cancelAddMovieDetailsBtn = addMovieModalDiv.querySelector('.btn--passive');
+const addMovieBtn = addMovieModalDiv.querySelector('.btn--success');
 const backdropDiv = document.getElementById('backdrop');
+const inputList = document.querySelector('.modal__content').querySelectorAll('input');
+
 
 const toggleBackdrop = () => {
     backdropDiv.classList.toggle('visible');
@@ -16,6 +19,27 @@ const backdropClickHandler = () => {
     toggleMovieModal();
 };
 
+const validateMovieDetails = () => {
+    const titleValue = inputList[0].value;
+    const imageURL = inputList[1].value;
+    const ratingValue = inputList[2].value;
+
+    if(
+        !titleValue.trim() || 
+        !imageURL.trim() || 
+        !ratingValue.trim() ||
+        +ratingValue < 1 ||
+        +ratingValue > 5
+    ) {
+        alert('please enter valid values');
+    } else {
+        toggleMovieModal();
+        return;
+    }
+
+};
+
 addMovieDetailsBtn.addEventListener('click', toggleMovieModal);
 backdropDiv.addEventListener('click', backdropClickHandler);
-cancelAddMovieBtn.addEventListener('click', toggleMovieModal);
+cancelAddMovieDetailsBtn.addEventListener('click', toggleMovieModal);
+addMovieBtn.addEventListener('click', validateMovieDetails);
