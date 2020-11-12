246b5ecdccd4958954eea339a73b5ac08a553ae7
Gab <gbraga@uwo.ca>
2020-11-12 15:00:50 -0500

    filter by title

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -4,7 +4,7 @@ const searchBtn = document.getElementById("search-btn");
 
 const movies = [];
 
-const renderMovies = () => {
+const renderMovies = (filter = '') => {
     const movieList = document.getElementById("movie-list");
     
     if (movies.length === 0) {
@@ -15,7 +15,10 @@ const renderMovies = () => {
     }
     // done this way to save time
     movieList.innerHTML = '';
-    movies.forEach(movie => {
+
+    const filteredMovies = !filter ? movies : movies.filter(movie => movie.info.title.includes(filter));
+
+    filteredMovies.forEach(movie => {
         const movieEl = document.createElement("li");
         let text = movie.info.title + ' - ';
         for (const key in movie.info) {
@@ -26,7 +29,7 @@ const renderMovies = () => {
         movieEl.textContent = text;
         movieList.append(movieEl);
     });
-}
+};
 
 const addMovieHandler = () => {
     const title = document.getElementById("title").value;
@@ -47,6 +50,12 @@ const addMovieHandler = () => {
 
     movies.push(newMovie);
     renderMovies();
-}
+};
+
+const searchMovieHandler = () => {
+    const filterTerm = document.getElementById("filter-title").value;
+    renderMovies(filterTerm);
+};
 
 addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMovieHandler);
