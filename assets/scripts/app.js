1ce61036a6a1e13f2c7d933f06ff49d3f81f932d
Gab <gbraga@uwo.ca>
2020-11-19 13:47:20 -0500

    added a ProductList class

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,12 @@
 class Product {
-    title = 'Default Title';
+ /*    title = 'Default Title';
     imageUrl;
     price;
-    description;
+    description; */
 
+    //adding properties on the fly in the constructor is perfectly fine
+    //you dont need to add them as fields beforehand, unless you want 
+    //to add a default value or something
     constructor(title, url, desc, price) {
         this.title = title;
         this.imageUrl = url;
@@ -11,42 +14,42 @@ class Product {
         this.price = price;
     }
 }
-
-const productList = {
-    products: [
+class ProductList {
+    products = [
         new Product(
             'A Pillow',
             'https://images.crateandbarrel.com/is/image/Crate/Emi20x20PlwCvrSHS20/?$web_zoom$&191122123350&wid=450&hei=450',
             'A soft pillow!',
             19.99),
         new Product(
-                'A Carpet',
-                'https://www.carpetandhome.com/wp-content/uploads/2020/05/1-3.jpg',
-                'A soft carpet!',
-                199.99)
-    ],
-      render() {
-          const appDiv = document.getElementById('app');
-          const productUl = document.createElement('ul');
-          productUl.className = 'product-list';
-          for (const product of this.products) {
-            const productLi = document.createElement('li');
-            productLi.className = 'product-item';
-            productLi.innerHTML = `
-            <div>
-                <img src="${product.imageUrl}" alt="${product.title}">
-                <div class="product-item__content">
-                    <h2>${product.title}</h2>
-                    <h3>\$${product.price}</h3>
-                    <p>${product.description}</p>
-                    <button>Add to Cart</button>
-                </div>
-            </div>
-            `;
-            productUl.append(productLi);
-          }
-          appDiv.append(productUl);
-      }
- };
+            'A Carpet',
+            'https://www.carpetandhome.com/wp-content/uploads/2020/05/1-3.jpg',
+            'A soft carpet!',
+            199.99)
+        ];
+        render() {
+            const appDiv = document.getElementById('app');
+            const productUl = document.createElement('ul');
+            productUl.className = 'product-list';
+            for (const product of this.products) {
+              const productLi = document.createElement('li');
+              productLi.className = 'product-item';
+              productLi.innerHTML = `
+              <div>
+                  <img src="${product.imageUrl}" alt="${product.title}">
+                  <div class="product-item__content">
+                      <h2>${product.title}</h2>
+                      <h3>\$${product.price}</h3>
+                      <p>${product.description}</p>
+                      <button>Add to Cart</button>
+                  </div>
+              </div>
+              `;
+              productUl.append(productLi);
+            }
+            appDiv.append(productUl);
+        }    
+}
 
-productList.render();
+const prodList = new ProductList();
+prodList.render();
