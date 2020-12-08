4bbbbfb1df748e3b4d5421452adc73e875d17481
Gab <gbraga@uwo.ca>
2020-12-08 14:40:09 -0500

    added logic for handler the project item switching lists

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,30 +1,52 @@
 class ToolTip {}
 
 class ProjectItem {
-    constructor(id) {
+    constructor(id, updateProjectListsFunction) {
         this.id = id;
+        this.updateProjectListsHandler = updateProjectListsFunction;
         this.connectMoreInfoBtn();
         this.connectSwitchBtn();
     }
 
     connectMoreInfoBtn() {
-
+        const projectItemLi = document.getElementById(this.id);
+        const moreInfoBtn = projectItemLi.querySelector('button:first-of-type');
+        //moreInfoBtn.addEventListener('click', )
     }
 
     connectSwitchBtn() {
         const projectItemLi = document.getElementById(this.id);
         const switchBtn = projectItemLi.querySelector('button:last-of-type');
-        switchBtn.addEventListener('click', )
+        switchBtn.addEventListener('click', this.updateProjectListsFunction);
     }
 }
 
 class ProjectList {
     projects = [];
+    projectType;
+    switchHandler;
+
     constructor(projectType) {
+        this.projectType = projectType;
         const projectItems = document.querySelectorAll(`#${projectType}-projects li`);
         for (const project of projectItems) {
-            this.projects.push(new ProjectItem(project.id));
+            this.projects.push(new ProjectItem(project.id, this.switchProject.bind(this)));
         }
+    }
+
+    addProject(project) {
+        this.projects.push(project);
+    }
+
+    switchProject(projectId) {
+        // returns an array of all the projects that DON'T have the passed 
+        // project id (effectively removing only that 1 project from the array)
+        this.switchHandler(this.projects.find(project => project.id === projectId));
+        this.projects = this.projects.filter(project => project.id !== projectId);
+    }
+
+    setSwitchHandlerFunction(switchHandler) {
+        this.switchHandler = switchHandler;
     }
 }
 
@@ -32,6 +54,8 @@ class App {
     static init() {
         const activeProjectsList = new ProjectList('active');
         const finishedProjectsList = new ProjectList('finished');
+        activeProjectsList.setSwitchHandlerFunction(finishedProjectsList.addProject.bind(finishedProjectsList));
+        finishedProjectsList.setSwitchHandlerFunction(activeProjectsList.addProject.bind(activeProjectsList));
     }
 }
 
